9b678957af1979d337e87be3fd1229dff1b7be5d
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-04-10 02:17:49 +0600

    <Toaster /> component implimented inside Layout; Admin.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,67 +1,68 @@
-import React, { useState } from 'react';
-import Header from '../Header/Header';
-import { Outlet, useLoaderData } from 'react-router-dom';
-import TShirt from '../T-Shirt/TShirt';
-import Cart from '../Cart/Cart';
-import './Home.css'
-import toast, { Toaster } from 'react-hot-toast';
+import React, { useState } from "react";
+import { Outlet, useLoaderData } from "react-router-dom";
+import TShirt from "../T-Shirt/TShirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
+import toast, { Toaster } from "react-hot-toast";
 
 const Home = () => {
-    const allTShirts = useLoaderData()
-    // console.log(allTShirts);
+  const allTShirts = useLoaderData();
+  // console.log(allTShirts);
 
-    // For 'Marbo ekhane' method use useState
-    const [cartContainer, setCartContainer] = useState([]);
+  // For 'Marbo ekhane' method use useState
+  const [cartContainer, setCartContainer] = useState([]);
 
-    // Creating click handler for buy button or add to cart button
-    const handleAddToCart = (addTShirt) => {
-        console.log(addTShirt);
+  // Creating click handler for buy button or add to cart button
+  const handleAddToCart = (addTShirt) => {
+    console.log(addTShirt);
 
-        const exist = cartContainer.find(item => item._id === addTShirt._id);
-        if (exist) {
-            toast('Item already added to cart')
-        }
-        else {
-            const newCartContainer = [...cartContainer, addTShirt]
-        setCartContainer(newCartContainer);
-        }
+    const exist = cartContainer.find((item) => item._id === addTShirt._id);
+    if (exist) {
+      toast("Item already added to cart");
+    } else {
+      const newCartContainer = [...cartContainer, addTShirt];
+      setCartContainer(newCartContainer);
+    }
 
-        /** 
-         * We need to add new item to cart
-         * As we can't push new item to 'state', we need to create a new array of items */
-        // const newCartContainer = [...cartContainer, addTShirt]
-        // setCartContainer(newCartContainer);
-    };
-    
-    // Remove item from cart Handler
-    const removeFromCartHandler = (removeTShirt) => {
-        // console.log(item);
-        const newCartContainer = cartContainer.filter(item => item._id !== removeTShirt);
-        setCartContainer(newCartContainer);
-    };
-    return (
-        <div className='homeContainer'>
-            <h2>This is Home page</h2>
-            <p>Total t-shirt: {allTShirts.length}</p>
+    /**
+     * We need to add new item to cart
+     * As we can't push new item to 'state', we need to create a new array of items */
 
-            {/* Use map() for loop through 'allTShirt' array */}
-            <div className="productContainer">
-            {
-                allTShirts.map((tShirt) => <TShirt
-                key = {tShirt._id}
-                tShirt = {tShirt}
-                handleAddToCart = {handleAddToCart}
-                ></TShirt>)
-            }
-            </div>
-            <div className="cartContainer">
-                <Cart
-                cartContainer = {cartContainer}
-                removeFromCartHandler = {removeFromCartHandler}
-                ></Cart>
-            </div>
-        </div>
+    // const newCartContainer = [...cartContainer, addTShirt]
+    // setCartContainer(newCartContainer); // Copied as else condition Ln 25
+  };
+
+  // Remove item from cart Handler
+  const removeFromCartHandler = (removeTShirt) => {
+    // console.log(item);
+    const newCartContainer = cartContainer.filter(
+      (item) => item._id !== removeTShirt
     );
+    setCartContainer(newCartContainer);
+  };
+  return (
+    <div className="homeContainer">
+      <h2>This is Home page</h2>
+      <p>Total t-shirt: {allTShirts.length}</p>
+
+      {/* Use map() for loop through 'allTShirt' array */}
+      <div className="productContainer">
+        {allTShirts.map((tShirt) => (
+          <TShirt
+            key={tShirt._id}
+            tShirt={tShirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
+        ))}
+      </div>
+      <div className="cartContainer">
+        <Cart
+          cartContainer={cartContainer}
+          removeFromCartHandler={removeFromCartHandler}
+        ></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
